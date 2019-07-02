440602bc5110ea93b5dc671d24b2a06d5c5ad040
Shaheryar <shaheryartariq909@gmail.com>
2019-07-02 16:36:51 +0500

    added otpWebcriptActivity.java registerWecriptActivity.java profileWebcriptActivity.java splashWebcriptActivity.java

---
+++ about_us_demo/app/src/main/java/com/example/zain/about_us_demo/registerWecriptActivity.java
@@ -0,0 +1,13 @@
+package com.example.zain.about_us_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registerWecriptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_wecript);
+    }
+}
